af8cb78dd12db56cb6549d5cb08aa254171c9258
Pavol Loffay <xloffay@nymfe38.fi.muni.cz>
2014-03-19 11:40:49 +0100

    logovanie

---
+++ trunk/src/main/java/cz/muni/fi/pv168/calendar/Main.java
@@ -0,0 +1,15 @@
+package cz.muni.fi.pv168.calendar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xloffay on 19.3.14.
+ */
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void Main(String[] args) {
+        log.info("Main method");
+    }
+}
