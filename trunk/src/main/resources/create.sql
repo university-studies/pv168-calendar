b742d315f784bd510015aad8aa1d9d52ae253f19
Pavol Loffay <xloffay@nymfe38.fi.muni.cz>
2014-03-26 11:49:43 +0100

    spring imlementation Event

--- trunk/src/main/resources/create.sql
+++ trunk/src/main/resources/create.sql
@@ -10,4 +10,4 @@ connect 'jdbc:derby://localhost:1527/calendar;user=user;password=user;create=tru
 CREATE TABLE EVENT (id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title VARCHAR(200) NOT NULL, description VARCHAR(800),location VARCHAR(500));
 insert into event (title,description,location) values('aaa','desc','trstena');
 
-CREATE TABLE USERS (id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, login VARCHAR(30) NOT NULL, hashedPassword VARCHAR(50), email VARCHAR(50));
+CREATE TABLE USERS (id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, name VARCHAR(30) NOT NULL, password VARCHAR(50), email VARCHAR(50));
