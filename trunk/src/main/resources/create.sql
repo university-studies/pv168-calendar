150781981f51eb3932a5375fa340fe13d5a18fe0
Pavol Loffay <p.loffay@gmail.com>
2014-03-26 01:55:45 +0100

    added sql commands

--- trunk/src/main/resources/create.sql
+++ trunk/src/main/resources/create.sql
@@ -6,6 +6,8 @@ CREATE TABLE "EVENT" (
 );
 
 
-
+connect 'jdbc:derby://localhost:1527/calendar;user=user;password=user;create=true';
 CREATE TABLE EVENT (id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, title VARCHAR(200) NOT NULL, description VARCHAR(800),location VARCHAR(500));
+insert into event (title,description,location) values('aaa','desc','trstena');
+
 CREATE TABLE USERS (id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY, login VARCHAR(30) NOT NULL, hashedPassword VARCHAR(50), email VARCHAR(50));
